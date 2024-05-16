69a429045b3ed2a2e432e6a46d8c502995384444
MoaadKa <moaadkarkouri@gmail.com>
2024-05-16 21:22:44 +0100

    first commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { HomeService } from './service/home.service';
 
@@ -7,26 +7,35 @@ import { HomeService } from './service/home.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [
-    IonicModule
-  ],
+  imports: [IonicModule],
 })
-export class HomeComponent  implements OnInit {
+export class HomeComponent implements OnInit {
+  constructor(private homeService: HomeService) {}
 
-  constructor(private homeService: HomeService) { }
-
-  ngOnInit() {}
-
+  public innerWidth: any;
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+  }
 
   onCkeck() {
-    this.homeService.getIp().subscribe(
-      (response) => {
-        if(response.ipAddress > 100) {
-          alert("OK")
-        } else {
-          alert("KO")
-        }
+    this.homeService.getIp().subscribe((response) => {
+      if (this.sum(response.ipAddress) > 100) {
+        alert('OK');
+      } else {
+        alert('KO');
       }
-    )
+    });
+  }
+  sum(str: string): number {
+    const str_: Array<string> = str.split('.');
+    let sum = 0;
+    str_.forEach((num) => {
+      sum += parseInt(num);
+    });
+    return sum;
   }
 }
